50e8d87cdb9c3e1301dfdeef0f5851032c19e5fb
Mantice <nicholas_call@outlook.com>
2023-08-18 04:52:55 -0400

    Commit #4 - 8/18/23

--- src/main/java/dev/paracausal/zetavouchers/api/requirements/Requirement.java
+++ src/main/java/dev/paracausal/zetavouchers/api/requirements/Requirement.java
@@ -2,18 +2,13 @@ package dev.paracausal.zetavouchers.api.requirements;
 
 import dev.paracausal.zetavouchers.api.ZetaVouchersAPI;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public abstract class Requirement {
 
     private final String key;
-    private final Map<String, RequirementOption> requirementOptions = new HashMap<>();
 
     public Requirement(String key) {
         this.key = key;
@@ -28,12 +23,6 @@ public abstract class Requirement {
         api.requirements().register(this);
     }
 
-    public List<RequirementOption> options() { return new ArrayList<>(requirementOptions.values()); }
-
-    public void addOption(RequirementOption option) {
-        requirementOptions.putIfAbsent(option.key(), option);
-    }
-
-    public abstract boolean allowed(Player player, String[] conditions);
+    public abstract boolean allowed(Player player, FileConfiguration config, String path);
 
 }
